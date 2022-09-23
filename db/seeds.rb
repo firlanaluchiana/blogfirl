a1b45439ded3f93617536a0754780c516a969904
Firlana LUchiana <frluchiana55@gmail.com>
2022-09-23 09:53:55 +0700

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user.create(email:"luchi@gmail.com", password:"qwerty", name:"luchi")
+User.create(email:"luchi@gmail.com", password:"qwerty", name:"luchi")
 
-post.create(title: "Anything", body:"free", user_id: User.first.id)
-
+Post.create(title: "Anything", body:"free", user_id: User.first.id)
